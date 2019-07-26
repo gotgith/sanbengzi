38a4f0e29d5cfb2d5c015d93863583af48a5f277
JH <18468107890@163.com>
2019-07-26 22:30:53 +0800

    使用chai.expect添加了测试用例，测试事件监听不会

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import Button from './button'
+import vm from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
-Vue.component('g-button', Button)
+Vue.component('g-button', vm)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 
@@ -16,3 +16,88 @@ new Vue({
     }
 
 })
+
+
+import chai from 'chai'
+const expect = chai.expect
+// 单元测试
+{
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+    vm.$el.remove()
+   vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(vm)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    vm.$on('click', function(){
+        expect(1).to.eq(1)
+    })
+    let button = vm.$el
+    button.click()
+}
--- src/button-group.vue
+++ src/button-group.vue
@@ -9,7 +9,7 @@
         mounted() {
             for (let node of this.$el.children){
                 let name = node.nodeName.toLowerCase()
-                if (name !== 'button'){
+                if (name !== 'vm'){
                     console.warn(`g-button-group的子元素应该全是g-button，但是你写的是${name}`)
                 }
             }
