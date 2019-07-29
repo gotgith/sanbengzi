1acb58287e23feb6ad4aac9c5c0670c73c6aba12
JH <18468107890@163.com>
2019-07-29 14:35:29 +0800

    添加了input.vue 及其样式

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,6 +6,7 @@
 
 <script>
     export default {
+        name: 'SanbengziButtonGroup',
         mounted() {
             for (let node of this.$el.children){
                 let name = node.nodeName.toLowerCase()
@@ -16,7 +17,7 @@
         }
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
     .g-button-group {
         display: inline-flex;
         vertical-align: top;
